b6f0fe40b477e1b51b3bc9950a01cce7d0891eac
Jordan de Souza <Jordan.de.Souza@ibm.com>
2020-03-03 15:21:37 -0500

    reformatted code

---
+++ src/App.components.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled"
+
+export const Container = styled.div`
+  width: 100vw
+  height 100vh
+
+  font-family: "Avenir Next";
+  font-size: 4vh;
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  padding-left: 100px
+`;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Range from "./Range";
+import RangeSlider from "./Range"
+import {Container} from "./App.components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <h1 style={{fontSize: "5vh"}}>Simple SPA</h1>
+          <ul className="header" >
+            <li><a href="/" style={{fontSize:"3vh"}}>Home</a></li>
+            <li><a href="/stuff" style={{fontSize:"3vh"}}>Stuff</a></li>
+            <li><a href="/contact" style={{fontSize:"3vh"}}>Contact</a></li>
+          </ul>
+          <div className="content">
+          </div>
+          <Range name="Openness" firstLabel="consistent and cautious" secondLabel="inventive and curious"/>
+    <Range name="Conscientiousness" firstLabel="easy-going and careless" secondLabel="efficient and organized"/>
+    <Range name="Extraversion" firstLabel="solitary and reserved" secondLabel="outgoing and energetic"/>
+    <Range name="Agreeableness" firstLabel="challenging and detached" secondLabel="friendly and compassionate"/>
+    <Range name="Neuroticism" firstLabel="secure and confident" secondLabel="sensitive and nervous"/>
+    </Container>
   );
 }
 
