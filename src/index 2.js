70eceec9e582dada84b8d61a7cbdac0c094f9e79
Nicole Beilis <Nicole.Beilis@ibm.com>
2020-03-03 15:05:59 -0500

    small change

--- src/index 2.js
+++ src/index 2.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import Range from "./Range";
-import RangeSlider from "./Range";
 
-ReactDOM.render(
+ReactDOM.render( //big five personality traits
   <div>
-    <Main/>
-    <Range name="Openness" firstLabel="consistent and cautious" secondLabel="inventive and curious"/>
+    <Range name="Openness" firstLabel="consistent and cautious" secondLabel="inventive and curious"/> 
     <Range name="Conscientiousness" firstLabel="easy-going and careless" secondLabel="efficient and organized"/>
     <Range name="Extraversion" firstLabel="solitary and reserved" secondLabel="outgoing and energetic"/>
     <Range name="Agreeableness" firstLabel="challenging and detached" secondLabel="friendly and compassionate"/>
