afc652130714aa2a3b430fdae3d515cc42269f12
Jordan de Souza <Jordan.de.Souza@ibm.com>
2020-03-04 00:13:01 -0500

    added early features and placeholders

--- src/App.components.js
+++ src/App.components.js
@@ -1,13 +1,65 @@
 import styled from "@emotion/styled"
 
 export const Container = styled.div`
-  width: 100vw
-  height 100vh
+  width: 100vw;
+  height 100vh;
 
   font-family: "Avenir Next";
   font-size: 4vh;
   display: flex;
+
   flex-direction: column;
   text-decoration: none;
-  padding-left: 100px
+
+  align-items:center;
+`;
+
+export const Banner = styled.div`
+  height: 20vh;
+  width: 100%;
+
+  background: #c3acce;
+
+  font-family: "Avenir Next";
+  font-size: 8vh;
+  font-style: bold;
+
+  align-items:center;
+  text-align:center;
+
+  display: flex;
+`;
+
+export const LottieDiv = styled.div`
+width: 100px;
+height:100px;
+padding-left: 30px;
+position: relative;
+`;
+
+export const Header = styled.h1`
+  font-size: 8vh;
+  font-style: bold;
+
+  position: relative;
+  padding-left: 20px;
+`;
+
+export const RangeBanner = styled.div`
+  height: 10vh;
+  width: 100%;
+`;
+
+export const Button = styled.div`
+height: 50px;
+width: 200px;
+font-size: 30px;
+
+border-radius: 10px;
+background: #ffdab9;
+color: #ffffff;
+
+text-align: center;
+
+
 `;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import Range from "./Range";
-import RangeSlider from "./Range"
-import {Container} from "./App.components"
+import React, {useState} from 'react';
+import Range from "./components/Range";
+import Lottie from "react-lottie";
+import rabbitData from "./Animations/bunny.json"
+import {Container, Banner, Header, Button, LottieDiv} from "./App.components"
+
+
+const bunnyLottie = {
+  loop: true,
+  autoplay: true,
+  animationData: rabbitData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 function App() {
+  const [pressed, setPressed] = useState(0);
   return (
     <Container className="App">
-      <h1 style={{fontSize: "5vh"}}>Simple SPA</h1>
+      <Banner>
+        <LottieDiv>
+          <Lottie
+            options={bunnyLottie}
+            height={"100px"}
+            width={"100px"}
+            isPaused={false}
+            />
+            </LottieDiv>
+        <Header>
+          <text>h</text>
+          <text style={{color:"#ffdab9"}}>A</text>
+          <text>ire</text>
+          </Header>
+      </Banner>
           <ul className="header" >
             <li><a href="/" style={{fontSize:"3vh"}}>Home</a></li>
             <li><a href="/stuff" style={{fontSize:"3vh"}}>Stuff</a></li>
@@ -19,6 +45,13 @@ function App() {
     <Range name="Extraversion" firstLabel="solitary and reserved" secondLabel="outgoing and energetic"/>
     <Range name="Agreeableness" firstLabel="challenging and detached" secondLabel="friendly and compassionate"/>
     <Range name="Neuroticism" firstLabel="secure and confident" secondLabel="sensitive and nervous"/>
+    <Button
+    style={{background: pressed ? "#c3acce" : "#ffdab9"}}
+    onMouseEnter={() => setPressed(!pressed)}
+    onMouseLeave={() => setPressed(!pressed)}
+    >
+      <text>search</text>
+      </Button>
     </Container>
   );
 }
